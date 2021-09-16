d986ab191dd66bec2031745827dc2b14fedb5be0
Karim Ryde <karye.webb@gmail.com>
2021-09-16 12:01:31 +0200

    Tränat på Tryparse(), if-satser

---
+++ Uppgift3/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows;
+
+namespace Uppgift3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void knappPlusClick(Object sender, RoutedEventArgs e)
+        {
+            // Flagga
+            bool ärKorrektInmattat = true;
+
+            // Töm resultatRutan
+            resultatRuta.Text = "";
+
+            // Försöker konvertera tal1 till en float
+            float tal1;
+            if (!float.TryParse( tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            // Försöker konvertera tal2 till en float
+            float tal2;
+            if (!float.TryParse( tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            if (ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} + {tal2} = {tal1 + tal2}";
+            }
+        }
+
+        private void knappMinusClick(Object sender, RoutedEventArgs e)
+        {
+            // Flagga
+            bool ärKorrektInmattat = true;
+
+            // Töm resultatRutan
+            resultatRuta.Text = "";
+
+            // Försöker konvertera tal1 till en float
+            float tal1;
+            if (!float.TryParse( tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            // Försöker konvertera tal2 till en float
+            float tal2;
+            if (!float.TryParse( tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            if (ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} - {tal2} = {tal1 - tal2}";
+            }
+        }
+
+        private void knappGångerClick(Object sender, RoutedEventArgs e)
+        {
+            // Flagga
+            bool ärKorrektInmattat = true;
+
+            // Töm resultatRutan
+            resultatRuta.Text = "";
+
+            // Försöker konvertera tal1 till en float
+            float tal1;
+            if (!float.TryParse( tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            // Försöker konvertera tal2 till en float
+            float tal2;
+            if (!float.TryParse( tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            if (ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} * {tal2} = {tal1 * tal2}";
+            }
+        }
+
+        private void knappDivideraClick(Object sender, RoutedEventArgs e)
+        {
+            // Flagga
+            bool ärKorrektInmattat = true;
+
+            // Töm resultatRutan
+            resultatRuta.Text = "";
+
+            // Försöker konvertera tal1 till en float
+            float tal1;
+            if (!float.TryParse( tal1Ruta.Text, out tal1))
+            {
+                MessageBox.Show("Tal1 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            // Försöker konvertera tal2 till en float
+            float tal2;
+            if (!float.TryParse( tal2Ruta.Text, out tal2))
+            {
+                MessageBox.Show("Tal2 fel inmatning.\n Formatet tex 2,5");
+                ärKorrektInmattat = false;
+            }
+
+            if (ärKorrektInmattat)
+            {
+                resultatRuta.Text = $"{tal1} / {tal2} = {tal1 / tal2}";
+            }
+        }
+    }
+}
