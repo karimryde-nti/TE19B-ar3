ee163ae5fef0a5a467599e742d98761298e57830
Karim Ryde <karye.webb@gmail.com>
2021-10-04 13:21:22 +0200

    Tränat på listor

---
+++ Listor/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en array
+            string[] städer = {"Berlin", "Paris", "London"};
+
+            // Ett annat att skapa en tom array
+            string[] frukter = new string[3];
+            frukter[0] = "Äpple";
+            frukter[1] = "Apelsin";
+            frukter[2] = "Banan";
+
+            // Skapa en lista av namn
+            List<string> namnen = new List<string>() {"Miriam", "Teo", "Axel"};
+
+            // Skapa en tom lista
+            List<string> bären = new List<string>();
+            bären.Add("Blåbär");
+            bären.Add("Lingon");
+            bären.Add("Hallon");
+            bären.Add("Björnbär");
+            bären.Add("Kråkbär");
+
+            // Skriv ut alla frukter arrayen
+            for (int i = 0; i < frukter.Length; i++)
+            {
+                Console.WriteLine(frukter[i]);
+            }
+            // En annan variant
+            foreach (var frukt in frukter)
+            {
+                Console.WriteLine(frukt);
+            }
+
+            // Skriv alla bären i Listan
+            for (int i = 0; i < bären.Count; i++)
+            {
+                Console.WriteLine(bären[i]);
+            }
+
+            // Om vi vill hitta en stad (array)
+            foreach (var stad in städer)
+            {
+                if (stad == "Paris")
+                {
+                    Console.WriteLine("Paris finns i arrayen");
+                }
+            }
+
+            // Om vi vill hitta ett bär (lista)
+            if (bären.Contains("Blåbär"))
+            {
+                Console.WriteLine("Blåbär finns i listan");
+            }
+
+            // Ta bort ett bär
+            bären.Remove("Hallon");
+            bären.RemoveAt(0);
+            bären.Insert(1, "Aroniabär");
+
+            foreach (var bär in bären)
+            {
+                Console.WriteLine(bär);
+            }
+        }
+    }
+}
---
+++ SamlaNamn/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamlaNamn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Global lista
+        List<string> namnlista = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickLäggTill(object sender, RoutedEventArgs e)
+        {
+            // Läs av rutaNamn
+            string namn = rutaNamn.Text;
+
+            // Om rutaNamn ej tom 
+            // Lägg till i rutaLista
+            // Och lagra i en lista
+            if (namn != "")
+            {
+                rutaLista.Text += $"{namn}\n";
+                namnlista.Add(namn);
+            }
+            else
+            {
+                rutaFel.Text = "Namn är tomt!";
+            }
+        }
+
+        private void ClickSpara(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines("namnlist.txt", namnlista);
+            rutaFel.Text = "Namnen sparade i namnlista.txt!";
+        }
+    }
+}
