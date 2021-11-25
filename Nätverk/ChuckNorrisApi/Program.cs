da65df126fa577a21d3f47a9bbc58433ffac241c
Karye <karye@users.noreply.github.com>
2021-11-25 11:19:24 +0100

    Tränat på REST och JSON

---
+++ Nätverk/ChuckNorrisApi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+
+namespace ChuckNorrisApi
+{
+    /* 
+    {
+        "icon_url" : "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "id" : "6181BpiWR_GTOJ1FhC1f5A",
+        "url" : "",
+        "value" : "Chuck Norris once traveled with the Ringling Bros and Barnum & Bailey Circus as the flaming chainsaw swallower."
+    }
+    */
+    class Joke
+    {
+        public string icon_url {get; set;}
+        public string value {get; set;}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuck Norris skämt");
+
+            // Hämta ett skämt från api.chucknorris.io
+            string url = "https://api.chucknorris.io/jokes";
+            var client = new RestClient(url);
+            var request = new RestRequest("random", DataFormat.Json);
+            var response = client.Get(request);
+
+            // Kolla på svaret
+            //Console.WriteLine(response.Content);
+
+            // Avkoda JSON-svaret
+            Joke skämtet = new Joke();
+            skämtet = JsonSerializer.Deserialize<Joke>(response.Content);
+
+            // Skriv ut skämtet
+            Console.WriteLine(skämtet.value);
+        }
+    }
+}
---
+++ Nätverk/PokeApi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace PokeApi
+{
+    /* 
+    {
+        "count": 1118,
+        "next": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+        "previous": null,
+        "results": [
+            {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+            },
+            ...
+        ]
+    }
+    */
+    class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+    class Pokemons
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hämta alla Pokemon");
+
+            // Hämta ett skämt från api.chucknorris.io
+            string url = "https://pokeapi.co/api/v2";
+            var client = new RestClient(url);
+            var request = new RestRequest("pokemon", DataFormat.Json);
+            var response = client.Get(request);
+
+            // Kollar på svaret
+            //Console.WriteLine(response.Content);
+
+            // Avkoda JSON-svaret
+            Pokemons lista = new Pokemons();
+            lista = JsonSerializer.Deserialize<Pokemons>(response.Content);
+
+            // Skriv ut alla pokemons
+            if (lista != null)  // Kolla att det gick att avkoda annars kraschar programmet
+            {
+                foreach (var item in lista.results)
+                {
+                    Console.WriteLine(item.name);
+                }
+            }
+
+        }
+    }
+}
