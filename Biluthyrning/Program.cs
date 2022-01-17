8c32556382d83bde30adb3e3043a395e67cf11bc
Karye <karye@users.noreply.github.com>
2022-01-17 13:34:05 +0100

    Skapat metoder

--- Biluthyrning/Program.cs
+++ Biluthyrning/Program.cs
@@ -1,55 +1,70 @@
-﻿// https://www.tutorialsteacher.com/csharp/csharp-dictionary
-
-using System.ComponentModel;
+﻿using System;
 using System.Collections.Generic;
-using System;
 
-namespace Biluthyrning
+namespace Biluthyrningen
 {
-    // Kostnad för bilarna
-    // Volvo ABC123 -> 500:-/d
-    // Saab DEF456 -> 400:-/d
-    // Fiat GHI789 -> 199:-/d
-    // Extra km = 2:-/km,
-    // Array? 
-    // [
-    //     ["ABC123", "500"], 
-    //     ["DEF456", "400"], 
-    //     ["GHI789", "199"]
-    // ]
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen till biluthyrningen!");
-            Console.WriteLine("Ange kundens personnr: ");
-            Console.WriteLine("Ange bilens regnr: ");
-            Console.WriteLine("Ange antal km: ");
-            Console.WriteLine("Ange antal dygn: ");
 
+            // Skapa ett första avtal = avta1
             Avtal avtal1 = new Avtal();
-            Console.WriteLine(avtal1._bilar["ABC123"]);
+
+            Console.Write("Ange kundens personnr: ");
+            avtal1.Personnr = Console.ReadLine();
+            Console.Write("Ange bilens regnr: ");
+            avtal1.RegNr = Console.ReadLine();
+            Console.Write("Ange antal km: ");
+            avtal1.Km = int.Parse(Console.ReadLine());
+            Console.Write("Ange antal dygn: ");
+            avtal1.Tidsram = int.Parse(Console.ReadLine());
+            avtal1.Datum = DateTime.Today;
+
+            // Räkna ut totala kostnaden
+            Console.WriteLine($"Total hyran blir {avtal1.RäknaKostnad()}:-");
+            Console.WriteLine($"Bilen skall åter {avtal1.RäknaInlämningsdatum()}");
         }
     }
 
     class Avtal
     {
         // Egenskaper
-        public DateTime Datum { get; set; }
-        public string RegNr { get; set; }
-        public int Km { get; set; }
-        public int Kostnad { get; set; }
-        public int Tidsram { get; set; }
-        public string Personnr { get; set; }
+        public string Personnr {get; set;}
+        public DateTime Datum {get; set;}
+        public string RegNr {get; set;}
+        public int Km {get; set;}
+        public int Kostnad {get; set;}
+        public int Tidsram {get; set;}
 
-        // Private variabel
-        public IDictionary<string, int> _bilar = new Dictionary<string, int>(){
+        // Privat variabler som bara ska användas inuti klassen
+        private IDictionary<string, int> _bilar = new Dictionary<string, int> {
             {"ABC123", 500},
-            {"DEF456", 400},
-            {"GHI789", 199}
+            {"DEF123", 400},
+            {"GHI123", 199}
         };
 
+        /// <summary>
+        /// Räkna ut bilens dyngskostnad * antal dygn + 2:-/km
+        /// </summary>
+        /// <returns></returns>
+        public int RäknaKostnad()
+        {
+            //Console.WriteLine(bilar["ABC123"]);           // => 500
+            // @todo inte krascha om regnr inte finns
+            // @todo 100 km ingår i hyran, bara betala för extra km
+            Kostnad = _bilar[RegNr] * Tidsram + Km * 2;     
+            return Kostnad;
+        }
 
+        /// <summary>
+        /// Räkna ut datum + tidsram
+        /// </summary>
+        /// <returns></returns>
+        public string RäknaInlämningsdatum()
+        {
+            return Datum.AddDays(Tidsram).ToString("dd MMMM yyyy");
+        }
     }
 }
